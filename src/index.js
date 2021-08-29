04c520f47c80482e0abfa86a1942916946ecf50a
Ihar Levachkov <pokerlis@gmail.com>
2021-08-29 22:22:20 +0300

    feat: add condition for brackets

--- src/index.js
+++ src/index.js
@@ -5,6 +5,34 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    let stack = [];
+    const openBr = "(";
+    const closedBr = ")";
+    let str;
+
+//---------------------------------Brackets-------------------------------------
+    for (let i = 0; i < expr.length; i++) {
+      let currentSymbol = expr[i];
+    
+      if (openBr === currentSymbol) {
+          stack.push(currentSymbol);
+      }
+      else {
+          if ((stack.length === 0) && (currentSymbol === closedBr)) {
+            throw ("ExpressionError: Brackets must be paired");
+          }
+  
+          let topElements = stack[stack.length - 1];
+          if ((topElements === openBr) && (currentSymbol === closedBr)) {
+              stack.pop();
+          }
+      }
+    }
+    if (stack.length !== 0) throw ("ExpressionError: Brackets must be paired");
+  //-----------------------------------------------------------------------------
+
+  
+
 }
 
 module.exports = {
